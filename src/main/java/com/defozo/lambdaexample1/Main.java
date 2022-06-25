fb4b97281e8a004662f1f70be3fe2e1529fac424
Michał Kiełtyka <defozo@gmail.com>
2022-06-25 11:47:21 +0200

    Initial commit

--- src/main/java/com/defozo/lambdaexample1/Main.java
+++ src/main/java/com/defozo/lambdaexample1/Main.java
@@ -1,2 +1,48 @@
-package com.defozo.lambdaexample1;public class Main {
+package com.defozo.lambdaexample1;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.AWSLambda;
+import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
+import com.amazonaws.services.lambda.model.InvokeRequest;
+import com.amazonaws.services.lambda.model.InvokeResult;
+import com.amazonaws.services.lambda.model.ServiceException;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String functionName = args[0];
+
+        //Generating unique ID:
+        String uniqueId = UUID.randomUUID().toString();
+
+        InvokeRequest invokeRequest = new InvokeRequest()
+                .withFunctionName(functionName)
+                .withPayload("{\n" +
+                        " \"uniqueId\": \"" + uniqueId + "\",\n" + //injecting unique ID into payload that we'll pass to another lambda
+                        "}");
+        InvokeResult invokeResult = null;
+
+        try {
+            AWSLambda awsLambda = AWSLambdaClientBuilder.standard()
+                    .withCredentials(new ProfileCredentialsProvider())
+                    .withRegion(Regions.US_EAST_1).build();
+
+            invokeResult = awsLambda.invoke(invokeRequest);
+
+            String ans = new String(invokeResult.getPayload().array(), StandardCharsets.UTF_8);
+
+            //write out the return value
+            System.out.println(ans);
+
+        } catch (ServiceException e) {
+            System.out.println(e);
+        }
+
+        System.out.println(invokeResult.getStatusCode());
+    }
+
 }
